b6da6fadacdc3b8ffcd5526a7122339753aa7e0d
samiwise <samiwise@192.168.1.18>
2018-10-28 20:23:54 +0000

    Created AddressDaoInterface class with addAddress() Method

---
+++ CA2LibrarySystem/src/interfaces/AddressDaoInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import Dtos.Address;
+
+/**
+ *
+ * @author samiwise
+ */
+public interface AddressDaoInterface {
+    public boolean addAddress (Address address);
+}
